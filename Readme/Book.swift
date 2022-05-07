c819cb8b296028a0bced2128e4557f10313ffce0
Uriel Enrique <urielenrique99@gmail.com>
2022-05-07 18:16:38 -0500

    table configuration and Book model

---
+++ Readme/Book.swift
@@ -0,0 +1,14 @@
+//
+//  Book.swift
+//  Readme
+//
+//  Created by Uriel Hernandez Gonzalez on 07/05/22.
+//
+
+import UIKit
+
+struct Book {
+    let title: String
+    let author: String
+    let image: UIImage
+}
